9b0d95eac72807e86bea59b776aed2a0db217436
MarcoMa <kindly.horse@gmail.com>
2015-01-12 17:49:55 +0800

    Created new class

--- Receiver/WorkerRole.cs
+++ Receiver/WorkerRole.cs
@@ -7,7 +7,7 @@
 // with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 // Unless required by applicable law or agreed to in writing, software distributed under the License is 
 // distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and limitations under the License.
+// See the License for the specific language governing permissions and limitations under the License. Test
 // ==============================================================================================================
 namespace Receiver
 {
@@ -24,7 +24,7 @@ namespace Receiver
     public class WorkerRole : RoleEntryPoint
     {
         private QueueManager queueManager;
-        private ManualResetEvent completedEvent = new ManualResetEvent(false);
+        private readonly ManualResetEvent completedEvent = new ManualResetEvent(false);
 
         public override void Run()
         {
